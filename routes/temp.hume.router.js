236c992b1eb44eca8764e34cf22bfc9658a90a08
Saul Tapia <rodrigo.1010stc@gmail.com>
2022-03-26 20:47:47 -0400

    detalles

--- routes/temp.hume.router.js
+++ routes/temp.hume.router.js
@@ -4,10 +4,11 @@ const router = express.Router();
 router.post("/", (req, res) => {
     const { temp, hume } = req.query;
     if (temp && hume) {
-        res.status(200).json({
-            temp,
-            hume,
-        });
+        // res.status(200).json({
+        //     temp,
+        //     hume,
+        // });
+        res.send('Recibido')
     } else {
         res.send("No existen parametros :v");
     }
